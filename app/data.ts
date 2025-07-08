59ac963b140e69485272562a3a99e831d3388ff8
Raunak Nitin <126315102+raunakg98@users.noreply.github.com>
2025-07-07 22:07:21 -0400

    testing blog connection and video on prod

--- app/data.ts
+++ app/data.ts
@@ -34,17 +34,17 @@ export const PROJECTS: Project[] = [
       'Advanced text summarizer built on a serverless architecture.',
     link: 'https://raunakg98.github.io/s3-summarizer/',
     video:
-      '/summarizer.gif',
+      '/summarizer.mp4',
     id: 'project1',
   },
-  {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    id: 'project2',
-  },
+  // {
+  //   name: 'Motion Primitives',
+  //   description: 'UI kit to make beautiful, animated interfaces.',
+  //   link: 'https://motion-primitives.com/',
+  //   video:
+  //     'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+  //   id: 'project2',
+  // },
 ]
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
@@ -89,26 +89,26 @@ export const BLOG_POSTS: BlogPost[] = [
     link: 'https://medium.com/@raunakghawghawe07/how-i-built-a-0-text-summarizer-that-saves-my-sanity-331cd4e96081',
     uid: 'blog-1',
   },
-  {
-    title: 'Why I left my job to start my own company',
-    description:
-      'A deep dive into my decision to leave my job and start my own company',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-2',
-  },
-  {
-    title: 'What I learned from my first year of freelancing',
-    description:
-      'A look back at my first year of freelancing and what I learned',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-3',
-  },
-  {
-    title: 'How to Export Metadata from MDX for Next.js SEO',
-    description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
-    link: '/blog/example-mdx-metadata',
-    uid: 'blog-4',
-  },
+  // {
+  //   title: 'Why I left my job to start my own company',
+  //   description:
+  //     'A deep dive into my decision to leave my job and start my own company',
+  //   link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+  //   uid: 'blog-2',
+  // },
+  // {
+  //   title: 'What I learned from my first year of freelancing',
+  //   description:
+  //     'A look back at my first year of freelancing and what I learned',
+  //   link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+  //   uid: 'blog-3',
+  // },
+  // {
+  //   title: 'How to Export Metadata from MDX for Next.js SEO',
+  //   description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
+  //   link: '/blog/example-mdx-metadata',
+  //   uid: 'blog-4',
+  // },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
